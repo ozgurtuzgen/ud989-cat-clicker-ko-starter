ee679db1fd53b244fcc3a9337d8ded72cab13ea6
Özgür TÜZGEN <otuzgen@stm.com.tr>
2018-01-02 19:54:24 +0300

    catlist implementations

--- js/app.js
+++ js/app.js
@@ -1,8 +1,46 @@
-var ViewModel = function () {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Toby');
-    this.imgSrc = ko.observable('');
-    this.imgAttribution = ko.observable('');
+var initialCats = [
+    {
+        name: 'Toby',
+        clickCount: 0,
+        imgSrc: '',
+        imgAttribution: '',
+        nicknames: ['a', 'b', 'c']
+    },
+    {
+        name: 'Teddy',
+        clickCount: 0,
+        imgSrc: '',
+        imgAttribution: '',
+        nicknames: ['dd', 'b', 'c']
+    },
+    {
+        name: 'Tikky',
+        clickCount: 0,
+        imgSrc: '',
+        imgAttribution: '',
+        nicknames: ['ss', 'b', 'c']
+    },
+    {
+        name: 'Tony',
+        clickCount: 0,
+        imgSrc: '',
+        imgAttribution: '',
+        nicknames: ['ff', 'b', 'c']
+    },
+    {
+        name: 'Puppy',
+        clickCount: 0,
+        imgSrc: '',
+        imgAttribution: '',
+        nicknames: ['gg', 'b', 'c']
+    }
+];
+
+var Cat = function (data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
     this.level = ko.computed(function () {
         if (this.clickCount() < 10) {
             return 'newbie';
@@ -14,10 +52,25 @@ var ViewModel = function () {
             return 'pro';
         }
     }, this);
+    this.nicknames = ko.observableArray(data.nicknames);
+};
 
+var ViewModel = function () {
+    var self = this;
+
+    this.catList = ko.observableArray([]);
+    initialCats.forEach(element => {
+        self.catList.push(new Cat(element));
+    });
+
+    this.currentCat = ko.observable(this.catList()[0]);
     this.incrementCounter = function () {
-        this.clickCount(this.clickCount() + 1);
-    }
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+    };
+
+    this.setCurrentCat = function (clickedCat) {
+        self.currentCat(clickedCat);
+    };
 };
 
 ko.applyBindings(new ViewModel());
